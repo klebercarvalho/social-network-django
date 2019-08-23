790af2ca7ffd318d4ad5b54fedac89ba33306643
Kleber Carvalho <klebercarvalho@MacBook-Pro.local>
2019-08-23 13:26:01 -0300

    Model, Mak Migrations, Migrate, Python Shell, Create, Select, Update Profile

--- socialnetwork/profiles/views.py
+++ socialnetwork/profiles/views.py
@@ -14,13 +14,20 @@ def index(request):
     return render(request, 'index.html')
 
 
-def show(request, profile_id): 
-    # print 'ID do perfil recebido: %s' % profile_id
-    profile = Profile()
-    if profile_id == '1':
-        profile = Profile('Kleber Carvalho', 'kleber.carvalho@gmail.com', '1111111', 'IBM')
-    
-    if profile_id == '2':
-        profile = Profile('Arthur Carvalho', 'arthur.carvalho@gmail.com', '2222222', 'Microsoft')
+def show(request, profile_id):
 
+    # print 'ID do perfil recebido: %s' % profile_id
+
+    # profile = Profile()
+
+    # if profile_id == '1':
+    #     profile = Profile('Kleber Carvalho', 'kleber.carvalho@gmail.com', '1111111', 'IBM')
+    # ''
+    # if profile_id == '2':
+    #     profile = Profile('Arthur Carvalho', 'arthur.carvalho@gmail.com', '2222222', 'Microsoft')
+    #
+    # if profile_id == '3':
+    #     profile = Profile('Marcia Carvalho', 'marcia.carvalho@gmail.com', '3333333', 'Red Hat')
+
+    profile = Profile.objects.get(id=profile_id)
     return render(request, 'profile.html', { "profile" : profile})
